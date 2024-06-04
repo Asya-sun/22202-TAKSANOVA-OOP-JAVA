a4384a877bae91647df0c659d0e46ba9d6b6c94b
Aleksandra Taksanova <asyasunsea@gmail.com>
2024-06-05 01:23:17 +0700

    clear project

--- factory/src/main/java/logic/warehouses/AutoWarehouse.java
+++ factory/src/main/java/logic/warehouses/AutoWarehouse.java
@@ -2,18 +2,12 @@ package logic.warehouses;
 
 import logic.details.Auto;
 import logic.factory_controllers.AutoWarehouseController;
+import logic.observer_pattern.Observable;
 
-public class AutoWarehouse<Auto> extends Warehouse<Auto>{
+public class AutoWarehouse<Auto> extends Warehouse<Auto> {
 
     public AutoWarehouse(int storageSize) {
         super(storageSize);
     }
 
-    /*
-    @Override
-    synchronized public Detail get() {
-        notify();
-        return super.get();
-    }
-     */
 }
